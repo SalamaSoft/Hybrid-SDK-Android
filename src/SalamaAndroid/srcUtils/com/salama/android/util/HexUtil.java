9c553d4fb9434262e33bd153979e58f94b7dfefe
Jack Liu <beef.liu@gmail.com>
2016-06-19 18:52:51 +0800

    add several methods of "toHexString"

--- src/SalamaAndroid/srcUtils/com/salama/android/util/HexUtil.java
+++ src/SalamaAndroid/srcUtils/com/salama/android/util/HexUtil.java
@@ -6,7 +6,36 @@ public class HexUtil {
 	 * 数值转16进制字符串
 	 * @param val 数值
 	 * @return 16进制字符串
-	 */
+	 */	
+	public static String toHexString(byte val) {
+		StringBuilder hexStr = new StringBuilder(Integer.toHexString(val));
+		if(hexStr.length() < 2) {
+			hexStr.insert(0, '0');
+		}
+		
+		return hexStr.substring(hexStr.length() - 2);
+	}
+
+	/**
+	 * 数值转16进制字符串
+	 * @param val 数值
+	 * @return 16进制字符串
+	 */	
+	public static String toHexString(short val) {
+		StringBuilder hexStr = new StringBuilder(Integer.toHexString(val));
+
+		for(int i = hexStr.length(); i < 4; i++) {
+			hexStr.insert(0, '0');
+		}
+		
+		return hexStr.substring(hexStr.length() - 4);
+	}
+
+	/**
+	 * 数值转16进制字符串
+	 * @param val 数值
+	 * @return 16进制字符串
+	 */	
 	public static String toHexString(int val) {
 		StringBuilder hexStr = new StringBuilder(Integer.toHexString(val));
 
@@ -16,14 +45,12 @@ public class HexUtil {
 		
 		return hexStr.toString();
 	}
-	/**
-	 * @return 16进制字符串
-	}
 
 	/**
 	 * 数值转16进制字符串
 	 * @param val 数值
 	 * @return 16进制字符串
+	 */	
 	public static String toHexString(long val) {
 		StringBuilder hexStr = new StringBuilder(Long.toHexString(val));
 
@@ -32,6 +59,15 @@ public class HexUtil {
 		}
 		
 		return hexStr.toString();
+	}
+
+	/**
+	 * byte数组转16进制字符串
+	 * @param val byte数组
+	 * @return 16进制字符串
+	 */
+	public static String toHexString(byte[] val) {
+		return toHexString(val, 0, val.length);
 	}
 	
 	/**
@@ -68,6 +104,119 @@ public class HexUtil {
 		}
 		
 		return hexStr.toString();
+	}
+
+	/**
+	 * 16进制字符串转byte数组
+	 * @param hexStr 16进制字符串
+	 * @return
+	 */
+	public static byte[] parseHexString(String hexStr) {
+		int byteLen = hexStr.length() / 2;
+		byte[] buffer = new byte[byteLen];
+		
+		parseHexString(hexStr, buffer, 0);
+		
+		return buffer;
+	}
+
+	/**
+	 * 16进制字符串转byte数组
+	 * @param hexStr 16进制字符串
+	 * @param destBuff 目标数组
+	 * @param offset 目标数组偏移
+	 */
+	public static void parseHexString(String hexStr, byte[] destBuff, int offset) {
+		int byteMax = offset + hexStr.length() / 2;
+		int beginIndex = 0;
+		for(int i = offset; i < byteMax; i++) {
+			destBuff[i] = (byte)(
+					hexCharToByte(hexStr.charAt(beginIndex + 1)) 
+					| ((hexCharToByte(hexStr.charAt(beginIndex)) << 4) & 0xf0)
+					);
+			
+			beginIndex += 2;
+		}
+	}
+
+	/**
+	 * 16进制字符转数值
+	 * @param c 16进制字符
+	 * @return 数值
+	 */
+	public static byte hexCharToByte(char c) {
+		if(c <= '9' && c >= '0') {
+			return (byte)(c - '0');
+		} else {
+			if(c >= 'a' && c <= 'f') {
+				return (byte)(c - 'a' + 10);
+			} else if(c >= 'A' && c <= 'F') {
+				return (byte)(c - 'A' + 10);
+			} else {
+				return 0;
+			}
+		}
+	}
+	
+	private static long[] BIT_AND_MASK_F_FOR_LONG = new long[]{
+		0xFL, 
+		0xF0L, 
+		0xF00L, 
+		0xF000L, 
+		0xF0000L, 
+		0xF00000L, 
+		0xF000000L, 
+		0xF0000000L, 
+		0xF00000000L, 
+		0xF000000000L, 
+		0xF0000000000L, 
+		0xF00000000000L, 
+		0xF000000000000L, 
+		0xF0000000000000L, 
+		0xF00000000000000L,
+		0xF000000000000000L
+	};
+	
+	/**
+	 * 16进制字符串转数值
+	 * @param hex 16进制字符串
+	 * @return 数值
+	 */
+	public static long hexToLong(String hex) {
+		int len = hex.length();
+		
+		long val = 0;
+		char c;
+		int bitOffset = 0;
+		int index = len - 1;
+		for(int i = 0; i < len; i++) {
+			c = hex.charAt(index);
+			val |= ((hexCharToLong(c) << bitOffset) & BIT_AND_MASK_F_FOR_LONG[i]);
+			
+			index --;
+			bitOffset +=4;
+		}
+
+		return val;
+	}
+	
+	/**
+	 * 16进制字符转数值
+	 * @param c 16进制字符
+	 * @return 数值
+	 */
+	public static long hexCharToLong(char c) {
+		if(c <= '9' && c >= '0') {
+			return (long)(c - '0');
+		} else {
+			if(c >= 'a' && c <= 'f') {
+				return (long)(c - 'a' + 10);
+			} else if(c >= 'A' && c <= 'F') {
+				return (long)(c - 'A' + 10);
+			} else {
+				return 0;
+			}
+		}
 	}
 
 }
